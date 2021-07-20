eaca9651c1ee29a7c937dd13f492b6e39bbac4c9
devil <562157205@qq.com>
2021-07-20 16:17:59 +0800

    update

---
+++ grow-initializer/src/main/java/com/devil/spring/DevilInitializer.java
@@ -0,0 +1,22 @@
+package com.devil.spring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * 初始化测试
+ *
+ * @author Devil
+ * @date Created in 2021/7/20 15:52
+ */
+public class DevilInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    private static final Logger log = LoggerFactory.getLogger(DevilInitializer.class);
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        log.info("DevilInitializer 进行初始化 ===================");
+    }
+}
---
+++ grow-listener/src/main/java/com/devil/spring/DevilListener.java
@@ -0,0 +1,20 @@
+package com.devil.spring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+/**
+ * @author Devil
+ * @date Created in 2021/7/20 16:08
+ */
+public class DevilListener implements ApplicationListener<ApplicationEvent> {
+
+    private static final Logger log = LoggerFactory.getLogger(DevilListener.class);
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        log.info("DevilListener 执行 ---------------------- 事件类型： {}", event);
+    }
+}
