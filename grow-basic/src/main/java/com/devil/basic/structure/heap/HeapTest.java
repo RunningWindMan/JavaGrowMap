cc8341506bdfc7aca9cc984ff8b29fb7eab7564c
devil <562157205@qq.com>
2021-07-21 18:04:13 +0800

    堆

---
+++ grow-basic/src/main/java/com/devil/basic/structure/heap/HeapTest.java
@@ -0,0 +1,127 @@
+package com.devil.basic.structure.heap;
+
+/**
+ * 堆测试
+ * 完全二叉树：
+ * <blockquote>
+ *        7
+ *        <br>
+ *    8       9
+ *    <br>
+ * 12  13  11
+ * </blockquote>
+ * <blockquote>
+ *     ||
+ *     <br>
+ *     V
+ * </blockquote>
+ * <blockquote>
+ * 数组： 7  8  9  12  13  11
+ * <br>
+ * 小标： 0  1  2  3   4   5
+ * </blockquote>
+ *
+ * @author Devil
+ * @date Created in 2021/7/21 16:47
+ */
+public class HeapTest {
+
+    public int left(int i) {
+        return (i + 1) * 2 - 1;
+    }
+
+    public int right(int i) {
+        return (i + 1) * 2;
+    }
+
+    public int parent(int i) {
+        // i为根结点
+        if (i == 0) {
+            return -1;
+        }
+        // (i - 1) >>> 1
+        return (i - 1) / 2;
+    }
+
+
+    /**
+     * 构造大顶堆
+     */
+    public void highestHeap(int[] a, int i, int heapLength) {
+        int l = left(i);
+        int r = right(i);
+        int largest = -1;
+        /**
+         * 下面两个if条件句用来找到三个元素中的最大元素的位置largest；
+         * l < heapLength说明l在数组内，i非叶子结点；
+         */
+        if (l < heapLength && a[i] < a[l]) {
+            largest = l;
+        } else {
+            largest = i;
+        }
+        // r < heapLength说明r在数组内
+        if (r < heapLength && a[largest] < a[r]) {
+            largest = r;
+        }
+        // 如果i处元素不是最大的，就把i处的元素与最大处元素交换，交换会使元素下降
+        if (i != largest) {
+            int temp = a[i];
+            a[i] = a[largest];
+            a[largest] = temp;
+            // 交换元素后，以a[i]为根的树可能不在满足大根堆性质，于是递归调用该方法
+            highestHeap(a, largest, heapLength);
+        }
+    }
+
+    /**
+     * 构造小顶堆
+     */
+    public void lowestHeap(int[] a, int i, int heapLength) {
+        int l = left(i);
+        int r = right(i);
+        int smallest = -1;
+        /*
+          下面两个if条件句用来找到三个元素中的最小元素的位置smallest；
+          s < heapLength说明l在数组内，i非叶子结点；
+         */
+        if (l < heapLength && a[i] > a[l]) {
+            smallest = l;
+        } else {
+            smallest = i;
+        }
+        // r < heapLength说明r在数组内
+        if (r < heapLength && a[smallest] > a[r]) {
+            smallest = r;
+        }
+        // 如果i处元素不是最小的，就把i处的元素与最小处元素交换，交换会使元素下降
+        if (i != smallest) {
+            int temp = a[i];
+            a[i] = a[smallest];
+            a[smallest] = temp;
+            // 交换元素后，以a[i]为根的树可能不在满足大根堆性质，于是递归调用该方法
+            lowestHeap(a, smallest, heapLength);
+        }
+    }
+
+    public void buildHighestHeap(int[] a, int heapLength) {
+        // 从后往前看，lengthParent - 1处的元素是第一个有孩子节点的节点
+        int lengthParent = parent(heapLength - 1);
+        // 最初，parent(length)之后的所有元素都是叶子结点；
+        // 因为大于length/2处元素的孩子节点如果存在，那么
+        // 它们的数组下标值必定大于length，这与事实不符；
+        // 在数组中，孩子元素必定在父亲元素的后面，从后往前
+        // 对元素调用highestHeap，保证了元素的孩子都是大根堆
+        for (int i = lengthParent; i >= 0; i--) {
+            highestHeap(a, i, heapLength);
+        }
+    }
+
+    public void buildLowestHeap(int[] a, int heapLength) {
+        int lengthParent = parent(heapLength - 1);
+        for (int i = lengthParent; i >= 0; i--) {
+            lowestHeap(a, i, heapLength);
+        }
+    }
+
+}
