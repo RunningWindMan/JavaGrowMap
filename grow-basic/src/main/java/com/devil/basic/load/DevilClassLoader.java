152438873a51b7f1440075546cdc34ad9f06191f
devil <562157205@qq.com>
2021-07-26 15:39:23 +0800

    自定义classloader

---
+++ grow-basic/src/main/java/com/devil/basic/load/DevilClassLoader.java
@@ -0,0 +1,38 @@
+package com.devil.basic.load;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * 自定义classloader
+ *
+ * @author Devil
+ * @date Created in 2021/7/26 13:47
+ */
+public class DevilClassLoader extends ClassLoader {
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        Class clazz = null;
+        String classFilename = name + ".class";
+        File classFile = new File(classFilename);
+        if (classFile.exists()) {
+            try (FileChannel fileChannel = new FileInputStream(classFile)
+                    .getChannel();) {
+                MappedByteBuffer mappedByteBuffer = fileChannel
+                        .map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+                byte[] b = mappedByteBuffer.array();
+                clazz = defineClass(name, b, 0, b.length);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (clazz == null) {
+            throw new ClassNotFoundException(name);
+        }
+        return clazz;
+    }
+}
