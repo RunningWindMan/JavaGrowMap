7802603f272d989437c2645d8dc59a8b8d3b8016
devil <562157205@qq.com>
2021-07-27 18:39:18 +0800

    reduce

---
+++ grow-basic/src/main/java/com/devil/basic/special/stream/ReduceTest.java
@@ -0,0 +1,32 @@
+package com.devil.basic.special.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Devil
+ * @date Created in 2021/7/27 17:14
+ */
+public class ReduceTest {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        Integer result = list.stream().reduce((d1, d2) -> {
+            System.out.println(d1);
+            System.out.println(d2);
+            System.out.println("=============");
+            if (d1 > d2) {
+                return d1;
+            } else {
+                return d2;
+            }
+        }).orElseGet(() -> 0);
+        System.out.println(result);
+    }
+
+}
