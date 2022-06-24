93cb38fde0f722fc7774d13d135113b323e53c94
devil <562157205@qq.com>
2022-06-24 09:53:25 +0800

    feat: ali ttl

--- grow-basic/src/main/java/com/devil/basic/thread/ThreadLocalTest.java
+++ grow-basic/src/main/java/com/devil/basic/thread/ThreadLocalTest.java
@@ -1,5 +1,11 @@
 package com.devil.basic.thread;
 
+import com.alibaba.ttl.TransmittableThreadLocal;
+import com.alibaba.ttl.threadpool.TtlExecutors;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * ThreadLocal测试
  *
@@ -30,10 +36,65 @@ public class ThreadLocalTest {
         }
     }
     
+    public void test2() {
+        ThreadLocal<String> threadLocal = new ThreadLocal<>();
+        InheritableThreadLocal<String> inheritableThreadLocal = new InheritableThreadLocal<>();
+    
+        threadLocal.set("threadLocal-value");
+        inheritableThreadLocal.set("inheritableThreadLocal-value");
+    
+        // 验证父子线程传递
+        new Thread(() -> {
+            System.out.println(threadLocal.get()); // null
+            System.out.println(inheritableThreadLocal.get()); //inheritableThreadLocal-value
+        }).start();
+    
+        threadLocal.remove();
+        inheritableThreadLocal.remove();
+    }
+    
+    public void test3() {
+        InheritableThreadLocal<String> inheritableThreadLocal = new InheritableThreadLocal<>();
+        // 验证线程池传递
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        for (int i = 0; i < 10; i++) {
+            inheritableThreadLocal.set("inheritableThreadLocal-value->" + i);
+            executorService.submit(() -> {
+                System.out.println(inheritableThreadLocal.get());
+            });
+        }
+        executorService.shutdown();
+        inheritableThreadLocal.remove();
+    }
+    
+    public void test4() {
+        InheritableThreadLocal<String> inheritableThreadLocal = new InheritableThreadLocal<>();
+        TransmittableThreadLocal<String> transmittableThreadLocal = new TransmittableThreadLocal<>();
+        // 验证线程池传递2
+        ExecutorService executorService2 = TtlExecutors.getTtlExecutorService(Executors.newFixedThreadPool(1));
+        for (int i = 0; i < 10; i++) {
+            inheritableThreadLocal.set("inheritableThreadLocal-value->" + i);
+            transmittableThreadLocal.set("transmittableThreadLocal-value->" + i);
+            executorService2.submit(() -> {
+                System.out.println(inheritableThreadLocal.get());
+                System.out.println(transmittableThreadLocal.get());
+            });
+        }
+        executorService2.shutdown();
+        inheritableThreadLocal.remove();
+        transmittableThreadLocal.remove();
+    }
+    
     public static void main(String[] args) {
         ThreadLocalTest test = new ThreadLocalTest();
-        test.test1();
-        test.test1();
+//        test.test1();
+//        test.test1();
+        
+//        test.test2();
+        
+//        test.test3();
+        
+        test.test4();
     }
     
 }
